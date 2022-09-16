ef9d1f18e32a3e995aa291e31122f73d73cc6ce9
kr96329 <hasmi5452@gmail.com>
2022-09-16 17:54:06 +0900

    set font and components... small refac

--- front/src/Pages/Home/index.js
+++ front/src/Pages/Home/index.js
@@ -1,13 +1,45 @@
-import React from "react";
+import * as React from "react";
+import { styled } from "@mui/system";
 
+import {
+  Container,
+  createTheme,
+  Grid,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from "@material-ui/core";
+
+const mtheme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      secondary: "#edf2ff",
+    },
+  },
+});
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 const Home = () => {
   return (
-    <div>
-      <h1>KOJ</h1>
-      <a href={"/login"}>login</a>
-      <br />
-      <a href={"/join"}>join</a>
-    </div>
+    <ThemeProvider theme={mtheme}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item theme={mtheme}>
+            <Typography style={{}}>나눔</Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={2}>
+          <Item theme={mtheme}>ds</Item>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 export default Home;
