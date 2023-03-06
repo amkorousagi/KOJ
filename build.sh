7279b20a92c27824bbf126c0af4cd538737190c1
amkorousagi <hasmi5452@gmail.com>
2023-03-06 16:42:43 +0900

    no cache build docker image

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build . --tag koj_front -f ./front/Dockerfile
-docker build . --tag koj_back -f ./back/Dockerfile
-docker build . --tag koj_koj -f ./koj/Dockerfile
-docker build . --tag koj_media -f ./media/Dockerfile
+docker build --no-cache . --tag koj_front -f ./front/Dockerfile
+docker build --no-cache . --tag koj_back -f ./back/Dockerfile
+docker build --no-cache . --tag koj_koj -f ./koj/Dockerfile
+docker build --no-cache . --tag koj_media -f ./media/Dockerfile
