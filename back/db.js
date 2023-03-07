1b744b7a943a596ca27588e21c0dbc6998a835fd
amkorousagi <hasmi5452@gmail.com>
2023-03-07 19:32:00 +0900

    small refac

--- back/db.js
+++ back/db.js
@@ -4,6 +4,7 @@ import { id, pwd, dbName, ip, port } from "./config";
 export async function connect_db() {
   try {
     await mongoose.connect(`mongodb://${id}:${pwd}@${ip}:${port}/${dbName}`, {
+      authSource: "admin",
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
