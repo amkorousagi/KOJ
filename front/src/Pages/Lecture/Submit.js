15ad9f60dffd96317e0525fe352733b6c7cf7ff5
amkorousagi <hasmi5452@gmail.com>
2023-08-30 13:40:48 +0900

    small refac

--- front/src/Pages/Lecture/Submit.js
+++ front/src/Pages/Lecture/Submit.js
@@ -245,10 +245,8 @@ const Submit = ({
           style={{
             textAlign: "center",
             position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
+            top: "50%",
+            left: "50%",
             transform: "translate(-50%, -50%)",
             maxHeight: "calc(100vh - 200px)",
             overflowY: "auto",
