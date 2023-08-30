f79cb18b1522af0447f5ca8ce332ae547ad2960d
amkorousagi <hasmi5452@gmail.com>
2023-08-30 14:39:24 +0900

    small refac

--- front/src/Pages/Score/index.js
+++ front/src/Pages/Score/index.js
@@ -150,10 +150,6 @@ const Score = () => {
                 <br />
                 <br />
                 <br />
-                <HighlightDiff
-                  answer={testcases[i].output_text}
-                  result={item.stdout[i] ? item.stdout[i] : "없음"}
-                />
                 <TextField
                   InputLabelProps={{ style: { color: "black" }, shrink: true }}
                   disabled
