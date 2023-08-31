5d123a0a47403d4573eacbd927dc958e023ad005
amkorousagi <hasmi5452@gmail.com>
2023-08-31 15:48:49 +0900

    no recoil

--- front/src/index.js
+++ front/src/index.js
@@ -2,13 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./Routes/index.js";
 
-import { RecoilRoot } from "recoil";
-
 const container = document.getElementById("root");
 // console.log = function () {}; // production not use console log
 const root = createRoot(container);
-root.render(
-  <RecoilRoot>
-    <Router />
-  </RecoilRoot>
-);
+root.render(<Router />);
