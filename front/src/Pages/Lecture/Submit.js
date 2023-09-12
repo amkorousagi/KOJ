83674a39041c930fe88af47132984323208d413e
amkorousagi <hasmi5452@gmail.com>
2023-09-12 14:54:43 +0900

    full width

--- front/src/Pages/Lecture/Submit.js
+++ front/src/Pages/Lecture/Submit.js
@@ -115,7 +115,8 @@ const Submit = ({
               size="small"
               multiline
               fullWidth
-              style={{ display: "inline" }}
+              placeholder="코드를 입력하세요!"
+              style={{ display: "inline", marginLeft: 5, marginRight: 5 }}
               onChange={(e) => {
                 const o = [...blank];
                 o[index] = e.target.value;
