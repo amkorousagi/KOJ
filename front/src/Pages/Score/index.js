af7e5ed336f02ebe22ece709836b7a8e292dd7d9
amkorousagi <hasmi5452@gmail.com>
2023-10-05 14:10:39 +0900

    @ for underflow output scores

--- front/src/Pages/Score/index.js
+++ front/src/Pages/Score/index.js
@@ -60,7 +60,7 @@ const HighlightDiff = ({ answer, result }) => {
               color: "red",
             }}
           >
-            {" "}
+            {"@"}
           </span>
         );
         continue;
