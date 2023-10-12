8ecd88f58dddfa089dbc88351875bce59f8e72f6
amkorousagi <hasmi5452@gmail.com>
2023-10-12 14:16:23 +0900

    monospace font

--- front/src/Pages/Score/index.js
+++ front/src/Pages/Score/index.js
@@ -73,7 +73,7 @@ const HighlightDiff = ({ answer, result }) => {
               color: "red",
             }}
           >
-            {"@"}
+            {"▯"}
           </span>
         );
         continue;
